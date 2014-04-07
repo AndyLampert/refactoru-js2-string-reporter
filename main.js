c1f38c86f401423b2a7f91cbf5b0d24b51bd122f
AndyLampert <lamperta@gmail.com>
2014-04-07 12:27:13 -0600

    through toUpperCase

---
+++ main.js
@@ -0,0 +1,10 @@
+var userInput = prompt("Enter something and I'll tell you all about it!");
+
+alert(
+	"You entered " + userInput + "!" + "\n" + 
+	"There are " + userInput.length + " characters in this word" +
+	"The third character you input is: " + userInput.charAt(2) + "\n" +
+	"The word in lowercase is " + userInput.toLowerCase() + "!" + "\n" +
+	"The word in uppercase is " + userInput.toUpperCase() + "!" + "\n" +
+	
+	);
